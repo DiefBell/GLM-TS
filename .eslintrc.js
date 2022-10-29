5cf0cc1d46173d5173db13e335bde531e6845c9c
diefbell <diefenbaker.bell@gmail.com>
2022-10-29 17:22:01 +0100

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2022,
+		sourceType: "module",
+		project: ["./tsconfig.json"]
+	},
+	plugins: [
+		"@typescript-eslint",
+	],
+    extends: [
+		"eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    rules: {
+        "prefer-const": "warn",
+        "@typescript-eslint/no-inferrable-types": "warn",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-empty-function": "off",
+		"brace-style": "off",
+		"@typescript-eslint/brace-style": [ "error", "allman", { "allowSingleLine": true } ],
+		"semi": "error",
+		"eol-last": "error"
+    }
+};
